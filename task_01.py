959fea4aba2472c7de5f96a3e7131abfb461b77d
Lalani Werake <lalanikw@gmail.com>
2015-02-28 01:05:30 +0000

    task_01.py

--- task_01.py
+++ task_01.py
@@ -4,6 +4,19 @@
 
 
 def know_what_i_mean(wink, numwink=2):
+    """this doc string strips winks and nudget and return retstr in a format.
+
+    Args:
+        wink (mixed): wink * by numwink.
+        numwink (int): input. default '2'
+
+    Returns:
+        str: all arguments concatenated with commas.
+
+    exampls:
+        >>>know_what_i_mean (hello, 2)
+        'Know what I mean? hello, 2'
+    """
     winks = (wink * numwink).strip()
     nudges = ('nudge ' * numwink).strip()
     retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
