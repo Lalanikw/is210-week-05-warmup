4611f36a52d536914b6d99717798b6b27b067a5b
Lalani Werake <lalanikw@gmail.com>
2015-02-28 05:13:05 +0000

    task_02.py

--- task_02.py
+++ task_02.py
@@ -4,3 +4,27 @@
 
 
 import hamlet
+
+def crazy_math(4, 100000, 8, 98):
+    """This function does crazy math using facts from hamlet file.
+
+    Args:
+        monkeys (int): Number of monkeys available.
+        hours (mixed): Number of hours available to produce Hamlet.
+        typewriters (int): Number of available typewriters (Optional).
+        bananas (int): Number of available bananas
+        POSITIONING (int): calculates the crazy math
+
+    Returns:
+        float: The percentage likelihood of producing Hamlet as a float.
+
+    Examples:
+
+        >>>
+    """
+    banana_effect = bananas * BANANA_MULTIFIER
+    utilization = monkeys / SHIFTS
+
+    POSITIONING = (hours * (utilization + banana_effect)) / HAMLETHOURS
+
+    return POSITIONING 
