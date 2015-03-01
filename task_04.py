69cbda6dd76a6f59dcce90b8dfec63a13fd7712b
Lalani Werake <lalanikw@gmail.com>
2015-03-01 18:57:45 +0000

    task_04.py

---
+++ task_04.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module does some pretty crazy math."""
+
+
+def too_many_kittens(kittens, litterboxes, catfood):
+
+    """game of kitten, litter boxes and food.
+
+    Args:
+        kittens (int): the number of kittens.
+        litterboxes (int): the number of available litterboxes.
+        catfood (boolean): weather or not any catfood exits.
+
+    Returns:
+        str: all arguments
+
+    Examples:
+        >>> too_many_kittens(12, 2, False)
+        False
+        >>> too_many_kittens (2, 13, True)
+        True
+
+    """
+    animal = litterboxes >= kittens and catfood
+    return animal
